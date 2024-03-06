4533726008dfac215b3ebc772cfe51431d59923a
James D. Mitchell <jdm3@st-andrews.ac.uk>
2024-03-06 12:22:21 +0000

    Add globals.h

---
+++ src/globals.h
@@ -0,0 +1,18 @@
+/*******************************************************************************
+**
+*A  perms.h                  permutations                     Julius Jonusas
+**                                                            James Mitchell
+**                                                            Wilf A. Wilson
+**                                                            Michael Young
+**
+**  Copyright (C) 2014-15 - Julius Jonusas, James Mitchell, Wilf A. Wilson,
+**  Michael Young
+**
+**  This file is free software, see the digraphs/LICENSE.
+**
+*******************************************************************************/
+// TODO update names
+
+#include <stdint.h>  // for uint16_t
+
+extern uint16_t HOMOS_DATA_CURRENT_NUM_NODES;
